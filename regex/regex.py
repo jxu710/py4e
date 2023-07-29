841029585e270bc9ab99b30957ba76f281fa879b
Jingru Xu <nicoxu1000@gmail.com>
2023-07-28 20:22:05 -0400

    string extraction

--- regex/regex.py
+++ regex/regex.py
@@ -3,5 +3,11 @@ import re
 x = "From: using the : character"
 y = re.findall("^F.*:", x)
 z = re.findall("^F.*?:", x)
-print(y)
-print(z)
+# print(y)
+# print(z)
+
+
+line = "From jingru@email.com Fri July"
+text = re.findall("\S+@\S+", line)
+text1 = re.findall("^From (\S*@*\S)", line)
+print(text1)
