60fdb6a764c9ca226df3b2b9826d4cdb194e24df
Jingru Xu <nicoxu1000@gmail.com>
2023-07-30 01:48:18 -0400

    Find bix number in file using regex

--- regex/maxNum.py
+++ regex/maxNum.py
@@ -1,5 +1,13 @@
 import re
 
 hand = open("mbox-short.txt")
+
+numlist = list()
 for lines in hand:
-    print(lines)
+    lines = lines.rstrip()
+    stuff = re.findall("^X-DSPAM-Confidence:(.[0-9.]+)", lines)
+    if len(stuff) != 1:
+        continue
+    num = float(stuff[0])
+    numlist.append(num)
+print("Max:", max(numlist))
