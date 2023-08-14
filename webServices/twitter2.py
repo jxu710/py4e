2af7585d71aa339bb21caf0c614b3c00ef1610db
Jingru Xu <nicoxu1000@gmail.com>
2023-08-13 23:36:08 -0400

    done request data from twitter api

--- webServices/twitter2.py
+++ webServices/twitter2.py
@@ -14,3 +14,13 @@ while True:
     print("Retrieving", url)
     connection = urllib.request.urlopen(url)
     data = connection.read().decode()
+
+    headers = dict(connection.getheaders())
+    print("Remaining", headers["x-rate-limit-remaining"])
+    js = json.loads(data)
+    print(json.dumps(js, indent=4))
+
+    for u in js["users"]:
+        print(u["screen_name"])
+        s = u["status"]["text"]
+        print(" ", s[:50])
