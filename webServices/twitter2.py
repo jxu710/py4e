b2ce8612f2a2310cbfc547bf9048bee5e6010a88
Jingru Xu <nicoxu1000@gmail.com>
2023-08-13 23:11:49 -0400

    decode data from twitter api request

--- webServices/twitter2.py
+++ webServices/twitter2.py
@@ -3,3 +3,14 @@ import json
 import twurl  # Twitter URL library
 
 TWITTER_URL = "https://api.twitter.com/1.1/friends/list.json"
+
+while True:
+    print("")
+    acct = input("Enter Twitter Account:")
+    if len(acct) < 1:
+        break
+    url = twurl.augment(TWITTER_URL, {"screen_name": acct, "count": "5"})
+
+    print("Retrieving", url)
+    connection = urllib.request.urlopen(url)
+    data = connection.read().decode()
