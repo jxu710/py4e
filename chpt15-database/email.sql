404365a82e954d6fadddb7d323a81a045afd74f6
Jingru Xu <nicoxu1000@gmail.com>
2023-10-18 23:25:52 -0400

    Add some sql practice

--- chpt15-database/email.sql
+++
@@ -1,4 +0,0 @@
--- What SQL command would you use to retrieve all users that have the email address quincy@freecodecamp.org?
-
-
-SELECT * FROM Users WHERE email="quincy@freecodecamp.org"
---
+++ chpt15-database/practice.sql
@@ -0,0 +1,10 @@
+-- What SQL command would you use to retrieve all users that have the email address quincy@freecodecamp.org?
+
+
+SELECT * FROM Users WHERE email="quincy@freecodecamp.org"
+
+-- What does the INSERT command do in SQL?
+
+-- It defines a new row by listing the fields we want to include followed by the values we want placed in the new row.
+
+INSERT INTO Artists (name) values ("The Beatles")
